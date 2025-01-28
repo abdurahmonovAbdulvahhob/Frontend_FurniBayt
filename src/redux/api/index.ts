097d2e91f9ea820d30c3b67287db4af4d74d8789
mushtariy93 <abdurashidovamushtariykumush@gmail.com>
2025-01-28 09:41:53 +0500

    added tags to redux

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -3,10 +3,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const mainApi = createApi({
   reducerPath: "mainApi",
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
-<<<<<<< HEAD
-  tagTypes: ["Products","Wishlist",],
-=======
-  tagTypes: ["Products", "Category"],
->>>>>>> 823174b59bb7987cc24417593e381f782837ced1
+  tagTypes: ["Products", "Category","Wishlist"],
   endpoints: () => ({}),
 });
