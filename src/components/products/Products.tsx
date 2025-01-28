4ed8955753dd3fafb683b71d2f1cc601f5362921
mushtariy93 <abdurashidovamushtariykumush@gmail.com>
2025-01-28 09:56:26 +0500

    added product wishlist icon

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -28,40 +28,27 @@ const Products = ({ products }: ProductsProps) => {
         )}
       </div>
       <div className="grid grid-cols-4 gap-8 max-[1240px]:grid-cols-3 max-[990px]:grid-cols-2 max-[620px]:gap-2">
-
         {products && products.length > 0 ? (
-        products.map((product) => (
-          <div
-            key={product.id}
-            className="relative group overflow-hidden rounded-lg shadow-md"
-          >
-            {/* Product Image */}
-            <div className="relative w-full h-[301px] max-[620px]:h-[240px] max-[430px]:h-[200px]">
-              <img
-                onClick={() => navigate(`/product/${product.id}`)} // Navigate qo‘shildi
-                className="w-full h-full bg-no-repeat bg-center bg-cover"
-                src={`${product.image[0]}`}
-                alt={product.title}
-              />
-            </div>
+          products.map((product) => (
+            <div
+              key={product.id}
+              className="relative group overflow-hidden rounded-lg shadow-md"
+            >
+              {/* Product Image */}
+              <div className="relative w-full h-[301px] max-[620px]:h-[240px] max-[430px]:h-[200px]">
+                <img
+                  onClick={() => navigate(`/product/${product.id}`)} // Navigate qo‘shildi
+                  className="w-full h-full bg-no-repeat bg-center bg-cover"
+                  src={`${product.image[0]}`}
+                  alt={product.title}
+                />
+              </div>
 
-            {/* Hover Effect */}
-            {/* <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
-              <button className="bg-white text-black px-4 py-2 rounded-md">
-                Add to Cart
-              </button>
-            </div> */}
-
-              <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
-                <button className="bg-white text-black px-4 py-2 rounded-md">
-                  Add to Cart
+              <div>
+                <button className="absolute top-10 right-1 w-[30px] h-[30px] bg-white rounded-full flex items-center justify-center text-[20px]">
+                  <IoCartOutline />
                 </button>
-                <div>
-                  <button className="absolute top-10 right-1 w-[30px] h-[30px] bg-white rounded-full flex items-center justify-center text-[20px]">
-                    <IoCartOutline />
-                  </button>
-                  <Heart product={product} />
-                </div>
+                <Heart product={product} />
               </div>
 
               {/* Product Details */}
@@ -95,7 +82,5 @@ const Products = ({ products }: ProductsProps) => {
     </div>
   );
 };
-
-
 
 export default memo(Products);
--- src/redux/api/product-api.ts
+++ src/redux/api/product-api.ts
@@ -1,21 +1,16 @@
-<<<<<<< HEAD
-import { IGetResponseProducts, IProduct, IProductQuery } from '@/types'
-import { mainApi } from './index'
-=======
 import {
   IGetResponseProducts,
   IGetResponseSingleProduct,
   IProductQuery,
+  IProduct
 } from "@/types";
 import { mainApi } from "./index";
->>>>>>> 823174b59bb7987cc24417593e381f782837ced1
 
 const extendedApi = mainApi.injectEndpoints({
   endpoints: (build) => ({
     getProducts: build.query<IGetResponseProducts, IProductQuery>({
       query: (params) => ({
         url: "products",
-<<<<<<< HEAD
         method: "GET",
         params,
       }),
@@ -25,23 +20,16 @@ const extendedApi = mainApi.injectEndpoints({
         url: "product/:id",
         method: "GET",
         params,
-=======
-        method: "GET",
-        params,
       }),
     }),
     getSingleProduct: build.query<IGetResponseSingleProduct, any>({
       query: (id) => ({
         url: `products/${id}`,
         method: "GET",
->>>>>>> 823174b59bb7987cc24417593e381f782837ced1
       }),
     }),
   }),
 });
 
-<<<<<<< HEAD
-export const { useGetProductsQuery, useGetProductByIdQuery } = extendedApi;
-=======
-export const { useGetProductsQuery, useGetSingleProductQuery } = extendedApi;
->>>>>>> 823174b59bb7987cc24417593e381f782837ced1
+export const { useGetProductsQuery, useGetSingleProductQuery, useGetProductByIdQuery } = extendedApi;
+
